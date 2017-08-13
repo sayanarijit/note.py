d90e34e00118494a27adccc6300fa1941f3bdb9a
Arijit Basu <sayanarijit@gmail.com>
2017-08-13 22:54:34 +0530

    more handy

--- note.py
+++ note.py
@@ -1,231 +1,193 @@
-#!/usr/bin/python2
+#!/usr/bin/env python
 
+from __future__ import print_function
 import os
 import subprocess
-import re
 import sys
 
-# Configure -----------------------
 
-editor = "vim"
-notesDir = os.path.expanduser("~") + "/notes"
+# Configuration ----------------------------
 
-#----------------------------------
+EDITOR = "vim"
+STORAGE = os.path.expanduser("~") + "/notes"
+SCREEN_WIDTH = 100 # If not detected automatically
 
-def hr(c):
-    try:
-        rows, columns = os.popen('stty size', 'r').read().split()
-    except:
-        columns = 100
-    print "",
-    for j in range(0, int(columns)):
-        print "\b"+c,
-    print
+#-------------------------------------------
+
+
+def scan_dir():
+    if not os.path.isdir(STORAGE): os.makedirs(STORAGE)
+    filenames = [f for f in os.listdir(STORAGE) if os.path.isfile(STORAGE+"/"+f)]
+    contents = list(map(readfile, filenames))
+    return dict(zip(filenames, contents))
 
 
 def clr():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-def allLower(words):
-    lower = []
-    for word in words:
-        lower.append(word.lower())
-    return lower
-
-def center(line):
-    rows, columns = os.popen('stty size', 'r').read().split()
-    space = (int(columns) - len(line))/2
-    for j in range(0, int(space) / 2):
-        print " ",
-    print line
+def interact(prompt):
+    try:
+        inp = input(prompt).strip() if sys.version_info > (3, 0) else str(raw_input(prompt)).strip()
+        if inp:
+            return inp
+        else:
+            quit()
+    except:
+        quit()
 
 
-def createNote(words):
-    filename = re.sub("[^a-zA-Z0-9]","_",words)
-    filepath = notesDir + "/" + filename
+def select(choices):
+    print("-"*columns)
+    print(("   ".join([k+") "+v for k, v in choices.items() if type(k) != int])).center(columns))
+    print("-"*columns)
+    return interact("Search or select: ")
 
+def create_note(filename=None):
     clr()
+    if not filename: filename = interact("Enter title: ")
+    filepath = STORAGE + "/" + filename
+
     while os.path.exists(filepath):
         clr()
-        print "Note \'" +re.sub("[^a-zA-Z0-9]", " ",filename) + "\' already exists !"
-        newname = raw_input("Try another name: ")
-        filename = re.sub("[^a-zA-Z0-9]","_",newname)
-        filepath = notesDir + "/" + filename
+        print("Note '{}' already exists!".format(filename))
+        newname = interact("Try another title: ")
+        filename = " ".join(newname)
+        filepath = STORAGE + "/" + filename
 
-    subprocess.call([editor, filepath])
-    clr()
+    subprocess.call([EDITOR, filepath])
 
     if os.path.isfile(filepath):
-        displayNote(filename)
+        display_note(filename)
     else:
-        words = "search " + re.sub("[^a-zA-Z0-9]"," ",words)
-        interact(words.split())
+        print("No new file created!")
+        quit()
 
 
-def displayNote(filename):
-    filepath = notesDir + "/" + filename
-
+def display_note(filename):
     clr()
-    center("* " + re.sub('[^a-zA-Z0-9]', ' ',filename) + " *")
-    hr("*")
-    print
-    with open(filepath) as f:
-        content = f.read()
-        print str(content)
-    hr("=")
-    print "o) open in editor \t r) rename \t d) delete \t s) search again"
-    hr("=")
+    global docs
+    global found
+    filepath = STORAGE + "/" + filename
+    print(("   "+filename+"   ").center(columns, "="))
+    print("\n"+readfile(filename)+"\n")
 
-    try:
-        ans = raw_input("> ")
-    except:
-        print "\nInvalid input !"
-        exit(1)
+    choices = {"o": "open with "+EDITOR, "r": "rename",
+               "d": "delete", "b": "back", "q": "quit"}
+    ans = select(choices).lower()
 
-    if ans in ["o","O"]:
+    if ans == "o":
+        subprocess.call([EDITOR, filepath])
+        display_note(filename)
+    elif ans == "r":
         clr()
-        subprocess.call([editor, filepath])
-        displayNote(filename)
-    elif ans in ["r","R"]:
-        clr()
-        print "Note \'" + re.sub("[^a-zA-Z0-9]", "_", filename) + "\' already exists !"
-        newname = re.sub('[^a-zA-Z0-9]', '_', str(raw_input("Enter new name \t: ")))
-        while os.path.exists(notesDir+"/"+newname):
+        newname = interact("Enter new title: ")
+        while os.path.exists(STORAGE+"/"+newname):
             clr()
-            print "Note \'" + re.sub("[^a-zA-Z0-9]", "_",newname) + "\' already exists !"
-            newname = raw_input("Try another name: ")
-            newname = re.sub("[^a-zA-Z0-9]", "_", newname)
+            print("Note '{}' already exists!".format(filename))
+            newname = interact("Try another title: ")
 
-        os.rename(filepath, notesDir+"/"+newname)
-        displayNote(newname)
-    elif ans in ["d","D"]:
+        os.rename(filepath, STORAGE+"/"+newname)
+        display_note(newname)
+    elif ans == "d":
         clr()
-
-        try:
-            ans = raw_input("Delete file \'"+filepath+"\' [ y/N ] ? ")
-        except:
-            print "\nInvalid input !"
-            exit(1)
+        ans = interact("Delete file \'"+filepath+"\'? [ y/N ]: ")
         clr()
         if ans in ["y","Y"]:
             os.remove(filepath)
         if not os.path.isfile(filepath):
-            print "Deleted file \'"+filepath+"\'"
-        else:
-            displayNote(filename)
-    elif ans in ["s", "S"]:
-        clr()
-        words = "search "
-        try:
-            words = words + raw_input("Search for: ")
-        except:
-            print "\nInvalid input !"
-            exit(1)
-        clr()
-        interact(words.split())
-
-    return 0
-
-
-def initDir():
-    if not os.path.isdir(notesDir):
-        os.makedirs(notesDir)
-        if os.path.isdir(notesDir):
-            print "Created directory: " + notesDir
-        else:
-            print "Error: Could\'nt create directory: " + notesDir
-            exit(1)
-    return 0
-
-
-def printHelp():
-    print
-    print "Usage:\t\t./note.py KEYWORDS [e.g. ./note.py patching rhel kernel]"
-    print
-    return 0
-
-
-def search(args):
-    print "Searching for \t: " + str(args)
-    filenames = os.listdir(notesDir)
-    searched = os.listdir(notesDir)
-
-    for filename in filenames:
-        if filename.startswith('.'):
-            searched.remove(str(filename))
-            continue
-        for word in args:
-            if word.lower() in allLower(filename.split("_")):
-                continue
-            else:
-                filepath = notesDir + "/" + filename
-                with open(filepath) as f:
-                    content = f.read()
-                if word.lower() in allLower(content.split()):
-                    continue
-                elif filename in searched:
-                    searched.remove(str(filename))
-
-    return searched
-
-
-def interact(args):
-    del args[0]
-
-    initDir()
-
-    words = " ".join(args)
-
-    filenames = search(args)
-    clr()
-    i = 1
-    opts = ["1"]
-
-    if len(filenames) > 0:
-        print str(len(filenames)) + " note(s) found"
-        hr("-")
-        print
-        for filename in filenames:
-            print " \t" + str(i) + ") " + re.sub("[^a-zA-Z0-9]"," ",filename)
-            i = i+1
-            opts.append(str(i))
-        print
-
-    hr("=")
-    print "n) new note \t s) search again \t h) help"
-    hr("=")
-
-    try:
-        ans = raw_input("> ")
-    except:
-        print "\nInvalid input !"
-        exit(1)
-
-    if ans in opts:
-        clr()
-        displayNote(str(filenames[int(ans)-1]))
-        exit(1)
-    elif ans in ["n", "N"]:
-        clr()
-        createNote(words)
-        exit(1)
-    elif ans in ["s", "S"]:
-        clr()
-        words = "search "
-        try:
-            words = words + raw_input("Search for: ")
-        except:
-            print "\nInvalid input !"
-            exit(1)
-        clr()
-        interact(words.split())
-    elif ans in ["h", "H"]:
-        printHelp()
-        exit(1)
+            print("Deleted file: '{}'".format(filepath))
+            quit()
+        print("File not deleted!")
+        quit()
+    elif ans == "b":
+        display_search(found)
+    elif ans == "q":
+        quit()
     else:
-        exit(0)
+        found = search(ans.split())
+        display_search(found, ans)
 
 
-# Call
-interact(sys.argv)
+def display_help():
+    print()
+    print("Usage:\t\t./note.py KEYWORDS [e.g. ./note.py patching rhel kernel]")
+    print()
+    quit()
+
+
+def match_word(word, content):
+    content = content.replace("\n"," ").replace("\r"," ").replace("\t"," ")
+    return list(map(lambda x: word in x.lower(), content.lower().split())).count(True)
+
+
+def readfile(filename):
+    with open(STORAGE+"/"+filename) as f:
+        content = f.read()
+    return str(content)
+
+
+def search(words=[]):
+    global docs
+    global found
+    match_count = {k: sum([match_word(w, k+" "+v) for w in words]) for k,v in docs.items()}
+    found = [k for k,v in match_count.items() if v != 0]
+    return sorted(found, key=lambda x: match_count[x], reverse=True)
+
+
+def display_search(result, qry=None):
+    clr()
+    global docs
+    if len(result) == 0 and qry:
+        print("No match found...")
+        ans = interact("Create new note with title '{}'? [y/N]: ".format(qry)).lower()
+        if ans == "y":
+            create_note(qry)
+        return
+
+    print("Found total:",len(result))
+    print("="*columns)
+    i = 1
+
+    print()
+    notes = {}
+    for filename in result:
+        print("    "+str(i)+")", filename)
+        notes[str(i)] = filename
+        i+= 1
+    print()
+
+    choices = {"n": "new note", "a":"list all", "h": "help", "q": "quit"}
+    ans = select(choices)
+    if ans in list(map(str, range(1,len(result)+1))):
+        display_note(notes[ans])
+    elif ans == "a":
+        display_search(docs.keys())
+    elif ans == "q":
+        quit()
+    elif ans == "h":
+        display_help()
+    elif ans == "n":
+        create_note()
+        docs = scan_dir()
+    else:
+        found = search(ans.split())
+        display_search(found, ans)
+
+
+if __name__ == "__main__":
+    clr()
+    try:
+        rows, columns = map(int, os.popen('stty size', 'r').read().split())
+    except:
+        columns = SCREEN_WIDTH
+
+    docs = scan_dir()
+
+    if len(docs) == 0: create_note()
+
+    if len(sys.argv) > 1:
+        found = search(sys.argv[1:])
+        display_search(found, qry=" ".join(sys.argv[1:]))
+    else:
+        display_search(docs.keys())
